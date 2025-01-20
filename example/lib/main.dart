f3900bcacaea7023fe3819793bc3df28bff80830
Nehal Babu <102398951+inehalbabu@users.noreply.github.com>
2025-01-20 12:43:41 +0530

    added print only in debug mode and add pub ignore

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
- final String title;
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
